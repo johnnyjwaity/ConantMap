50ae84611710e9deb9c4d6e29de2ec7950857623
johnnyjwaity <johnnywaity@gmail.com>
2018-10-31 23:25:43 -0500

    Added API

---
+++ conant-api/app.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var fs = require('fs');
+
+app.use(express.static(__dirname + '/client'));
+
+app.get('/bug-report', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+
+});
+
+io.on('connection', function(socket){
+  console.log('user connected');
+
+  socket.on('bug report', function(data){
+    var date = new Date()
+    var timeString = date.toLocaleTimeString();
+    timeString = timeString.split(':').join('-');
+    var fileName = date.toDateString() + " " + timeString + ".txt"
+    var writeStream = fs.createWriteStream("reports/" + fileName);
+    writeStream.write(data.name + "\n\n" + data.email + "\n\n" + data.report)
+    writeStream.close();
+  });
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  })
+})
+
+http.listen(3000, function(){
+  console.log('listening on 3000');
+});
